8562ccfc4a1523baa833d965680bd618be825c04
Dds2 <85358022+dds256@users.noreply.github.com>
2024-08-23 14:36:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -53,7 +53,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/dds256/MusicBot",
+    "https://github.com/dds256/MyBot",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv(
